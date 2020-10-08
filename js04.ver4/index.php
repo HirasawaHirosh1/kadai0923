bbe71d49b4553a3e9950fa7656ce85f8a0c888cb
HirasawaHirosh1 <69379427+HirasawaHirosh1@users.noreply.github.com>
2020-10-08 23:45:39 +0900

    Update index.php

--- js04.ver4/index.php
+++ js04.ver4/index.php
@@ -1,58 +1,59 @@
 <html>
   <head></head>
   <body>
-    <!--
-    BEFORE RUNNING:
-    ---------------
-    1. If not already done, enable the Google Sheets API
-       and check the quota for your project at
-       https://console.developers.google.com/apis/api/sheets
-    2. Get access keys for your application. See
-       https://developers.google.com/api-client-library/javascript/start/start-js#get-access-keys-for-your-application
-    3. For additional information on authentication, see
-       https://developers.google.com/sheets/api/quickstart/js#step_2_set_up_the_sample
-    -->
+
     <script>
-    function makeApiCall() {
-      var params = {
-        // The ID of the spreadsheet to retrieve data from.
-        spreadsheetId: '1OYL9SREyA0AvAbc-WCLgRe7x_dySfDPnb481BglbCQM',  // TODO: Update placeholder value.
-
-        // The A1 notation of the values to retrieve.
-        range: 'Sheet1',  // TODO: Update placeholder value.
-
-        // How values should be represented in the output.
-        // The default render option is ValueRenderOption.FORMATTED_VALUE.
-        //valueRenderOption: '',  // TODO: Update placeholder value.
-
-        // How dates, times, and durations should be represented in the output.
-        // This is ignored if value_render_option is
-        // FORMATTED_VALUE.
-        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-        //dateTimeRenderOption: '',  // TODO: Update placeholder value.
-      };
-
-      var request = gapi.client.sheets.spreadsheets.values.get(params);
+    function makeApiCall(action="read") {
+     
+     
+        var ssId = 'xxx;
+  var rng = 'Sheet1'; 
+ // Writing to sheet
+  if(action=="write"){
+   var vals = new Array(6);
+   for (var row= 0; row <6; row++){
+   vals[row] = new Array(3); // new row with 3 columns
+   for (var col= 0; col <3; col++){
+   vals[row][col]= document.getElementById(row+":"+col).value;
+    }
+   }
+   var params = {
+   spreadsheetId: ssId,
+   range: rng,
+   valueInputOption: 'RAW',
+   };
+   
+  var valueRangeBody ={"values": vals};
+ 
+      var request = gapi.client.sheets.spreadsheets.values.update(params, valueRangeBody);
       request.then(function(response) {
-        // TODO: Change code below to process the `response` object:
+       
         console.log(response.result);
       }, function(reason) {
         console.error('error: ' + reason.result.error.message);
       });
     }
+ else{   //Reading from Sheet
+  var params = {
+   spreadsheetId: ssId,
+   range: rng,
+   };
+  var request = gapi.client.sheets.spreadsheets.values.get(params);
+      request.then(function(response) {
+        // TODO: Change code below to process the `response` object:
+        console.log(response.result);
+  populateSheet(response.result);
+      }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+      });
+    }
+ }
+ function initClient() {
+      var API_KEY = 'yyy';  
+      var CLIENT_ID = 'zzz'; 
 
-    function initClient() {
-      var API_KEY = 'AIzaSyCZIpdUblYESIEmROwC2EMdnrwtRi6fQSY';  // TODO: Update placeholder with desired API key.
 
-      var CLIENT_ID = '959118115283-34km09skvuaov3bpc46r68kg77ejv489.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
-
-      // TODO: Authorize using one of the following scopes:
-      //   'https://www.googleapis.com/auth/drive'
-      //   'https://www.googleapis.com/auth/drive.file'
-      //   'https://www.googleapis.com/auth/drive.readonly'
-      //   'https://www.googleapis.com/auth/spreadsheets'
-      //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-      var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+      var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
 
       gapi.client.init({
         'apiKey': API_KEY,
@@ -82,12 +83,39 @@
     function handleSignOutClick(event) {
       gapi.auth2.getAuthInstance().signOut();
     }
+ 
+  function handleSaveClick() {
+      makeApiCall(action="write");
+    } 
+ function populateSheet(result) {
+   for(var row=0; row< 6; row++) {       //row<result.values.length
+  for(var col=0; col< 3; col++) {           //col<result.values[0].length;
+  document.getElementById(row+":"+col).value = result.values[row][col];
+  }
+
+   }
+ }
+ 
     </script>
     <script async defer src="https://apis.google.com/js/api.js"
       onload="this.onload=function(){};handleClientLoad()"
       onreadystatechange="if (this.readyState === 'complete') this.onload()">
     </script>
-    <button id="signin-button" onclick="handleSignInClick()">Sign in</button>
-    <button id="signout-button" onclick="handleSignOutClick()">Sign out</button>
+   <button id="signin-button" onclick="handleSignInClick()">Sign in</button>
+    <button id="signout-button" onclick="handleSignOutClick()">Sign out</button> 
+ <button id="save-button" onclick="handleSaveClick()">Save</button> 
+
+ 
+  <div style="margin-left:auto; margin-right:auto; width:1960px;">
+    <?php
+    for($row = 0; $row < 6; $row++) {
+      echo "<div style='clear:both'>";
+      for($col = 0; $col < 3; $col++) {
+        echo "<input type='text' style='float:left;' name = '$row:$col' id='$row:$col'>";
+      }
+      echo "</div>";
+    }
+    ?>
+ </div>
   </body>
 </html>
